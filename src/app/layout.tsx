7109e93700fc88101f6c14d34d54211922e369d0
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-06-06 18:44:41 +0600

    fav fixing for vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
